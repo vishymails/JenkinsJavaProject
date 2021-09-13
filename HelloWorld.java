90d9b8abfc85bc0df5bda6ce14d3c6f684656f5b
vishymails <vishymails@gmail.com>
2021-09-13 10:38:17 +0530

    added java code for Jenins

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] s) {
+        System.out.println("Hello IBM");
+        for(int i = 0; i<=10; i++) {
+            System.out.println("Value of I : " + i);
+        }
+    }
+}
