5ce5a19f443dd8ed4eea3f3a5c5dd33ebb475235
Vishwanath Rao <vishymails@gmail.com>
2022-06-21 15:33:23 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] s) {
-        System.out.println("Hello IBM");
+        System.out.println("Hello Oracle");
         for(int i = 0; i<=10; i++) {
             System.out.println("Value of I : " + i);
         }
