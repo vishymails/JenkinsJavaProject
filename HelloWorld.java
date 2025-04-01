54e088d17b03036efffe2e437b8498b6dfd5c01d
Dr. Vishwanath Rao <vishymails@gmail.com>
2025-04-01 16:36:18 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,10 @@
 public class HelloWorld {
     public static void main(String[] s) {
-        System.out.println("Hello Oracle - India Devops Session ");
+        System.out.println("Hello Oracle - India Devops Session - Bangalore ");
 
         for(int i = 0; i<=10; i++) {
             System.out.println(i);
+            
         }
     }
 }
